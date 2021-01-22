a80aa7414e46a8f39a7ca1cf5a365869a985401f
Mackaber <x@mackaber.me>
2021-01-21 19:08:27 -0600

    Version

--- App.tsx
+++ App.tsx
@@ -4,7 +4,22 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import Navigation from './navigation';
 
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAyvNWwnZaxu1InlMiNiuRVsqn9JbvttM0",
+  authDomain: "navigation-85a0f.firebaseapp.com",
+  databaseURL: "https://navigation-85a0f-default-rtdb.firebaseio.com",
+  projectId: "navigation-85a0f",
+  storageBucket: "navigation-85a0f.appspot.com",
+  messagingSenderId: "136690178681",
+  appId: "1:136690178681:web:4b52b369cb22eac9de92a8"
+};
+
 export default function App() {
+
+  firebase.initializeApp(firebaseConfig);
+
   return (
     <SafeAreaProvider>
       <Navigation />
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { Button, Text } from 'react-native-paper';
+import * as firebase from 'firebase';
+
+const newFirebaseRecord = () => {
+  firebase
+  .database()
+  .ref('records/' + 1)
+  .set({
+    record: "LOL Ima record"
+  });
+}
 
 const Home = ({navigation}) => {
+
+  mRecord = new BehaviorSubject('')
+  const record = useObservableState(mRecord)
+  
+  firebase.database().ref('records/' + 1).on('value', (snapshot) => {
+    mRecord.next(snapshot.val().record)
+  });
+  
   return(
     <View>
       <Text>Hello World!</Text>
+
+      <Text>Record:</Text>
+      <Text>{{record}}</Text>
+
+
+      <Button onPress={ () => newFirebaseRecord() }>
+        <Text>Magic!</Text>
+      </Button>
     </View>
   );
 }
